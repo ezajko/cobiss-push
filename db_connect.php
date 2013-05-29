9c32126c58f4c6d8b6c1ec024097ba757b14bfcc
Matjaz <matjaz@novisplet.com>
2013-05-29 12:17:14 +0200

    ignore

--- db_connect.php
+++ db_connect.php
@@ -15,13 +15,15 @@ class DB_Connect {
     // Connecting to database
     public function connect() {
        // require_once 'config.php';
-	   $DB_HOST= "localhost";
+	    require_once 'config.php';
+	    /*
+		$DB_HOST= "localhost";
 		$DB_USER= "root";
 		$DB_PASSWORD= "q1w2e3";
 		$DB_DATABASE= "push";
-
+        */
         // connecting to mysql
-        $con = mysql_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
+        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 		
         // selecting database
         mysql_select_db($DB_DATABASE);
