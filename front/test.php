db53f4ec80e2921aa6a27010ac2722bf03c39599
mmmatjaz <mmmatjaz@gmail.com>
2013-05-30 01:04:38 +0200

    first @ home

---
+++ front/test.php
@@ -0,0 +1,9 @@
+﻿<?php
+    require_once '../db_functions_gcm.php';
+ 
+    $gcm = new DbFunctionsGCM();
+    $gcm->storeUser("sikmggfhfhshjhjbvaab", "1105zz23207", "fklgjdfszzzgglgklmmmsdčf");
+    
+    //echo $result;
+
+?>
