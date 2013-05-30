0bfc09e67d21c8f9bb03deb47b2e0f4e010ae8b4
mmmatjaz <mmmatjaz@gmail.com>
2013-05-31 00:20:29 +0200

    there there

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "push");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "BIzaSyCRLa4LQZWNQBcRCYcIVYA45i9i8zfClqc"); // Place your Google API Key
+?>
