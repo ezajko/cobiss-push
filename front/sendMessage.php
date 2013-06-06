557487f2920fa835e7eee231991723489dfd0065
Matjaz <matjaz@novisplet.com>
2013-06-06 12:18:04 +0200

    messages by token

--- front/sendMessage.php
+++ front/sendMessage.php
@@ -9,7 +9,7 @@
 	
 	$com=new DbFunctionsCommon();
 	$members=$com->getAllUsers();
-	if ($users==0 || mysql_num_rows($users)==0) print 'no devices';
+	if (mysql_num_rows($users)==0) print 'no devices';
 	else {
 		//save message
 		$gcm = new DbFunctionsGCM();
