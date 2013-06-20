28fc947e57c2b785c76a07a5bcc77908ebfe8122
Matjaz <matjaz@novisplet.com>
2013-06-20 17:19:13 +0200

    meh

---
+++ ApnsPHP/send_count_apn.php
@@ -0,0 +1,21 @@
+<?php
+//header('Content-Type: text/html; charset=utf-8');
+echo 'vrh';
+if (isset($_GET["token"]) && isset($_GET["badge"]) ) {
+    
+	$token = $_GET["token"];
+	$badge = $_GET["badge"];
+	
+	print "badge: ".$badge." token: ".$token;
+    /*
+	include_once '../APN.php';
+	*/
+	include_once '../ApnsPHP/pushOne.php';
+	    
+	echo pushOne($token, $badge);
+	
+	
+	//echo 'aafaf';
+    
+}
+?>
