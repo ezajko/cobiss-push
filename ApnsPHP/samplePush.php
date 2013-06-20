67d2bb00f31f5ffe18ac2aef45796ba0ce83cf2e
Matjaz <matjaz@novisplet.com>
2013-06-20 17:34:18 +0200

    meh

--- ApnsPHP/samplePush.php
+++ ApnsPHP/samplePush.php
@@ -42,9 +42,9 @@ $push->setRootCertificationAuthority('../ApnsPHP/entrust.pem');
 
 // Connect to the Apple Push Notification Service
 $push->connect();
-
+$token='124526cdb84970eac30592ee434657b7141895bf9b92ff8e1d2fbb2d14d56559';
 // Instantiate a new Message with a single recipient
-$message = new ApnsPHP_Message('124526cdb84970eac30592ee434657b7141895bf9b92ff8e1d2fbb2d14d56559');
+$message = new ApnsPHP_Message(token);
 
 // Set a custom identifier. To get back this identifier use the getCustomIdentifier() method
 // over a ApnsPHP_Message object retrieved with the getErrors() message.
