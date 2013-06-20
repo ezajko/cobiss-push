63a96a0e6ea026ecd9faf415f6df1502007050ab
Matjaz <matjaz@novisplet.com>
2013-06-20 19:11:50 +0200

    meh

--- gcmApi/definitions.php
+++ gcmApi/definitions.php
@@ -1,7 +1,7 @@
 <?php
 
 define("WELCOME_TITLE", "Prijava na obvestila"); // Place your Google API Key
-define("WELCOME_TEXT", "Naroèeni ste na obvestila za èlanstvo "); // Place your Google API Key
+define("WELCOME_TEXT", "Naroceni ste na obvestila za clanstvo "); // Place your Google API Key
 
 
 ?>
