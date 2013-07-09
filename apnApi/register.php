3a07618bc6eafeba5b984b05ba9e201499b6b577
matjaz <mmmatjaz@gmail.com>
2013-07-09 21:59:22 +0200

    attempt

--- apnApi/register.php
+++ apnApi/register.php
@@ -7,8 +7,9 @@
 header('Content-Type: text/xml; charset=utf-8');
 print '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
 print '<APN>';
- if (isset($_REQUEST["acr"]) && isset($_REQUEST["memId"]) && isset($_REQUEST["regId"])) {
-    $acr = $_REQUEST["acr"];
+ if (isset($_REQUEST["acr"]) &&  isset($_REQUEST["memId"]) &&  isset($_REQUEST["regId"]) &&
+    !empty($_REQUEST["acr"]) && !empty($_REQUEST["memId"]) && !empty($_REQUEST["regId"])) {
+   $acr = $_REQUEST["acr"];
     $memid = $_REQUEST["memId"];
     $token = $_REQUEST["regId"]; // GCM Registration ID
     // Store user details in db
