db1004dd8e963f5ab7eef3f836f0f146a36a27a3
matjaz <mmmatjaz@gmail.com>
2013-07-10 13:46:53 +0200

    testing

--- ApnsPHP/CobissAPN.php
+++ ApnsPHP/CobissAPN.php
@@ -67,7 +67,7 @@ class CobissAPN {
 				$message = new ApnsPHP_Message($ids[$i]);
 				$message->setCustomIdentifier("Message-Badge-".$badge);
 				$message->setBadge(1*($badges[$i]));
-				$message->setText('Imate neprebrana sporocila.');
+				$message->setText('Imate neprebrana sporoèila.');
 				print $message->getText();
 				$message->setSound();
 				$message->setCustomProperty('acme2', array('bang', 'whiz'));
