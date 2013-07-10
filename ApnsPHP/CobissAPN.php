57d4bff28488a1e1d902819f2ffe79b8806a46bf
matjaz <mmmatjaz@gmail.com>
2013-07-10 12:11:31 +0200

    testing

--- ApnsPHP/CobissAPN.php
+++ ApnsPHP/CobissAPN.php
@@ -17,11 +17,10 @@ class CobissAPN {
 		try {
 			$message = new ApnsPHP_Message($token);
 			$message->setCustomIdentifier("Message-Badge-".$badge);
-			$message->setText(
-					'Naroèeni ste na obvestila.');//.$memid.'@'.$acr.'.');
+			$message->setText('Naroèeni ste na obvestila.');//.$memid.'@'.$acr.'.');
 			$message->setSound();
-			$message->setCustomProperty('acme2', array('bang', 'whiz'));
-			$message->setCustomProperty('acme3', array('bing', 'bong'));
+			//$message->setCustomProperty('acme2', array('bang', 'whiz'));
+			//$message->setCustomProperty('acme3', array('bing', 'bong'));
 			$message->setExpiry(30);
 			$push->add($message);
 			$push->send();
@@ -68,9 +67,9 @@ class CobissAPN {
 				$message->setBadge(1*($badges[$i]));
 				$message->setText('Imate neprebrana sporoèila.');
 				//$message->se
-				//$message->setSound();
-				$message->setCustomProperty('acme2', array('bang', 'whiz'));
-				$message->setCustomProperty('acme3', array('bing', 'bong'));
+				$message->setSound();
+				//$message->setCustomProperty('acme2', array('bang', 'whiz'));
+				//$message->setCustomProperty('acme3', array('bing', 'bong'));
 				$message->setExpiry(30);
 				$push->add($message);
 			} catch (Exception $e) {
@@ -80,12 +79,14 @@ class CobissAPN {
 			
 		}
 		
-		
-		// Send all messages in the message queue
-		$push->send();
-		print "\nSent";
-		// Disconnect from the Apple Push Notification Service
-		$push->disconnect();
+		try {
+			$push->send();
+			print "\nSent";
+			// Disconnect from the Apple Push Notification Service
+			$push->disconnect();
+		} catch (Exception $e) {
+			
+		}
 		print "\nDisconnected";
 		$aErrorQueue = $push->getErrors();
 		if (!empty($aErrorQueue)) {
